628562ea820669c885e2b5601521c24f962a7934
Jacob Yim <jacobyim@berkeley.edu>
2021-04-10 18:07:59 -0700

    Added paint system

---
+++ Assets/Scripts/PaintPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintPickup : MonoBehaviour {
+    public int paintValue;
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") {
+
+            Debug.Log("Picked up Paint");
+
+            col.gameObject.GetComponent<PaintResource>().AddPaint(paintValue);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PaintSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintSpawn : MonoBehaviour {
+    public GameObject paintObj;
+
+    public void SpawnNew() {
+
+        Debug.Log("New Paint spawned");
+
+        Instantiate(paintObj, transform.position, Quaternion.identity);
+    }
+}
