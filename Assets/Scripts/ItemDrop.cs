1c57f91ac03319eae237f44fced842429f5050dc
Jacob Yim <jayimgs@gmail.com>
2021-04-25 05:17:16 -0700

    progress on combat lvl 1

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour {
+    public List<GameObject> items = new List<GameObject>();
+
+    public void DropAll() {
+        if (items != null) {
+            for (int i = 0; i < items.Count; i++) {
+                Instantiate(items[i], transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/KeyItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    public string _color;
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag("Player"))
+        {
+            target.GetComponent<PlayerInventory>().addItem(_color + " Key");
+            Destroy(gameObject);
+        }
+    }
+}
